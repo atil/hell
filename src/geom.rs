4230f679523058aa9f6e07c7dc2bb665f1c52b43
atil <atil@torrenglabs.com>
2020-03-16 00:36:36 +0100

    Update geom calculations

--- src/geom.rs
+++ src/geom.rs
@@ -8,37 +8,39 @@ pub struct Triangle {
     pub normal: Vector3<f32>,
 }
 
-#[derive(Clone, Copy)]
-pub struct Plane {
-    a: f32,
-    b: f32,
-    c: f32,
-    d: f32,
+pub fn point_triangle_plane_distance(point: Point3<f32>, triangle: Triangle) -> f32 {
+    Vector3::dot(point - triangle.p0, triangle.normal)
 }
 
-pub fn get_plane(point: Point3<f32>, normal: Vector3<f32>) -> Plane {
-    Plane {
-        a: normal.x,
-        b: normal.y,
-        c: normal.z,
-        d: Vector3::dot(EuclideanSpace::to_vec(point), normal),
-    }
-}
-
-pub fn point_plane_distance(point: Point3<f32>, plane: Plane) -> f32 {
-    (plane.a * point.x + plane.b * point.y + plane.c * point.z - plane.d).abs()
-        / (plane.a * plane.a + plane.b * plane.b + plane.c * plane.c).sqrt()
-}
-
-pub fn point_triangle_distance(point: Point3<f32>, triangle: Triangle) -> f32 {
-    let plane = get_plane(triangle.p0, triangle.normal);
-    point_plane_distance(point, plane)
-}
-
-pub fn project_point_on_plane(point: Point3<f32>, plane: Plane) -> Point3<f32> {
-    todo!()
+pub fn project_point_on_triangle_plane(point: Point3<f32>, triangle: Triangle) -> Point3<f32> {
+    point + (point_triangle_plane_distance(point, triangle) * -triangle.normal)
 }
 
 pub fn is_point_in_triangle(point: Point3<f32>, tri: Triangle) -> bool {
-    todo!()
+    let c1 = Vector3::cross(point - tri.p0, tri.p1 - tri.p0);
+    let c2 = Vector3::cross(point - tri.p1, tri.p2 - tri.p1);
+    let c3 = Vector3::cross(point - tri.p2, tri.p0 - tri.p2);
+
+    c1.y > 0.0 && c2.y > 0.0 && c3.y > 0.0
+}
+
+pub fn point_line_segment_distance(point: Point3<f32>, p0: Point3<f32>, p1: Point3<f32>) -> f32 {
+    // TODO: Lots of sqrt here
+    let dot = Vector3::dot(point - p0, p1 - p0);
+    let line_segment_length = (p1 - p0).magnitude();
+    if 0.0 < dot && dot < line_segment_length {
+        // on the line segment
+        let line_segment_dir = (p1 - p0).normalize();
+        let point_on_line = p0 + dot * line_segment_dir;
+        return (point - point_on_line).magnitude();
+    } else {
+        // not on the segment, take the shorter one
+        let dist1 = (point - p0).magnitude();
+        let dist2 = (point - p1).magnitude();
+        if dist1 < dist2 {
+            return dist1;
+        } else {
+            return dist2;
+        }
+    }
 }
--- src/physics.rs
+++ src/physics.rs
@@ -28,19 +28,23 @@ pub fn step(objects: &Vec<Object>, player_pos: Point3<f32>) -> (Vector3<f32>, bo
     (total_displacement, is_grounded)
 }
 
-fn resolve(player_shape: PlayerShape, tri: Triangle) -> Vector3<f32> {
-    let plane = get_plane(tri.p0, tri.normal);
-    let dist1 = point_plane_distance(player_shape.capsule0, plane);
-    let dist2 = point_plane_distance(player_shape.capsule1, plane);
-    let (closer_point, dist_to_plane) = match dist1 < dist2 {
+fn resolve(player_shape: PlayerShape, triangle: Triangle) -> Vector3<f32> {
+    let dist1 = point_triangle_plane_distance(player_shape.capsule0, triangle);
+    let dist2 = point_triangle_plane_distance(player_shape.capsule1, triangle);
+    let (closer_point, closer_dist_to_plane) = match dist1 < dist2 {
         true => (player_shape.capsule0, dist1),
         false => (player_shape.capsule1, dist2),
     };
 
-    let point_on_plane = project_point_on_plane(closer_point, plane);
-    let (penet, dir) = match is_point_in_triangle(point_on_plane, tri) {
-        true => (dist_to_plane, tri.normal),
+    if closer_dist_to_plane > player_shape.radius {
+        return Vector3::<f32>::zero();
+    }
+
+    let point_on_plane = project_point_on_triangle_plane(closer_point, triangle);
+    let (penet, dir) = match is_point_in_triangle(point_on_plane, triangle) {
+        true => (closer_dist_to_plane, triangle.normal),
         false => todo!(),
     };
+
     Vector3::zero()
 }
