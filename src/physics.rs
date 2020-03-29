89cfeda79956572186cb484fc5f73f7289cececb
atil <atil@torrenglabs.com>
2020-03-29 01:12:59 +0100

    Fix penetration reference point
    Also, add some tests

--- src/physics.rs
+++ src/physics.rs
@@ -32,6 +32,10 @@ pub fn step(objects: &Vec<Object>, player_pos: Point3<f32>) -> (Vector3<f32>, bo
     let mut total_displacement = Vector3::<f32>::zero();
     for obj in objects {
         for tri in &obj.mesh.triangles {
+            let penet = compute_penetration(player_shape, *tri);
+            if penet.y < 0.0 {
+                println!("+++++++ {:?}\n {:?} \n {:?}", penet, player_shape, *tri);
+            }
             total_displacement += compute_penetration(player_shape, *tri);
         }
     }
@@ -42,15 +46,31 @@ pub fn step(objects: &Vec<Object>, player_pos: Point3<f32>) -> (Vector3<f32>, bo
 fn compute_penetration(player_shape: PlayerShape, triangle: Triangle) -> Vector3<f32> {
     let dist1 = point_triangle_plane_distance(player_shape.capsule0, triangle);
     let dist2 = point_triangle_plane_distance(player_shape.capsule1, triangle);
-    let (closer_point, closer_dist_to_plane) = match dist1.partial_cmp(&dist2) {
-        Some(Ordering::Less) => (player_shape.capsule0, dist1),
-        Some(Ordering::Greater) => (player_shape.capsule1, dist2),
+
+    let (closer_point, closer_tip_point, closer_dist_to_plane) = match dist1.partial_cmp(&dist2) {
+        Some(Ordering::Less) => (
+            player_shape.capsule0,
+            player_shape.capsule0 + Vector3::unit_y() * player_shape.radius,
+            dist1,
+        ),
+        Some(Ordering::Greater) => (
+            player_shape.capsule1,
+            player_shape.capsule1 - Vector3::unit_y() * player_shape.radius,
+            dist2,
+        ),
         Some(Ordering::Equal) => {
             let mid_point =
                 player_shape.capsule0 + (player_shape.capsule1 - player_shape.capsule0) * 0.5;
-            (mid_point, dist1)
+            (
+                mid_point,
+                mid_point + triangle.normal * player_shape.radius,
+                dist1,
+            )
         }
-        None => panic!(format!("Invalid comparison {} and {}", dist1, dist2)),
+        None => panic!(format!(
+            "Invalid capsule point comparison {} and {}",
+            dist1, dist2
+        )),
     };
 
     if closer_dist_to_plane > player_shape.radius {
@@ -59,10 +79,7 @@ fn compute_penetration(player_shape: PlayerShape, triangle: Triangle) -> Vector3
 
     let point_on_plane = project_point_on_triangle_plane(closer_point, triangle);
     match is_point_in_triangle(point_on_plane, triangle) {
-        true => {
-            (closer_dist_to_plane + player_shape.radius)
-                * (point_on_plane - closer_point).normalize()
-        }
+        true => (closer_tip_point - point_on_plane).magnitude() * triangle.normal,
         false => {
             let (p1, d1) =
                 get_closest_point_on_line_segment(point_on_plane, triangle.p0, triangle.p1);
@@ -98,6 +115,46 @@ mod tests {
         assert_eq!(
             compute_penetration(player_shape, tri),
             Vector3::new(0.0, 0.75, 0.0)
+        );
+    }
+
+    #[test]
+    fn test_resolve_2() {
+        let player_shape = PlayerShape {
+            capsule0: Point3::new(0.0, 1.47, 0.0),
+            capsule1: Point3::new(0.0, 0.47, 0.0),
+            radius: 0.5,
+        };
+
+        let tri = Triangle::new(
+            Point3::new(29.0, 1.0, -29.0),
+            Point3::new(-29.0, 1.0, -29.0),
+            Point3::new(-29.0, 1.0, 29.0),
+        );
+
+        assert_eq!(
+            compute_penetration(player_shape, tri),
+            Vector3::new(0.0, 0.97, 0.0)
+        );
+    }
+
+    #[test]
+    fn test_resolve_3() {
+        let player_shape = PlayerShape {
+            capsule0: Point3::new(0.0, 1.9283209, 0.0000009736774),
+            capsule1: Point3::new(0.0, 0.9283209, 0.0000009736774),
+            radius: 0.5,
+        };
+
+        let tri = Triangle::new(
+            Point3::new(29.0, 1.000002, -29.0),
+            Point3::new(-29.0, 1.000002, -29.0),
+            Point3::new(-29.0, 0.999998, 29.0),
+        );
+
+        assert_eq!(
+            compute_penetration(player_shape, tri),
+            Vector3::new(0.0, 1.0 - 0.9283209 + 0.5, 0.0)
         );
     }
 
