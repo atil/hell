34d22f555010dbcfc9c7d8eec27b48235fcc138b
atil <hyperion91@gmail.com>
2020-06-22 18:22:41 +0200

    Make point light static
    We set the shader variables once in the initialization. We won't have dynamic point lights for some time

--- src/point_light.rs
+++ src/point_light.rs
@@ -7,9 +7,10 @@ pub struct PointLight {
     pub position: Point3<f32>,
     pub intensity: f32,
     pub attenuation: f32,
+    pub depth_cubemap_handle: u32,
+
     fbo: u32,
     shader: Shader,
-    pub depth_cubemap_handle: u32,
 }
 
 impl PointLight {
@@ -33,6 +34,56 @@ impl PointLight {
             gl::ReadBuffer(gl::NONE);
             gl::DrawBuffer(gl::NONE);
             gl::BindFramebuffer(gl::FRAMEBUFFER, 0);
+
+            let proj = cgmath::perspective(
+                cgmath::Deg(90.0),
+                render::SHADOWMAP_SIZE as f32 / render::SHADOWMAP_SIZE as f32,
+                render::NEAR_PLANE,
+                render::FAR_PLANE,
+            );
+
+            let pos = position;
+
+            let v0 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(1.0, 0.0, 0.0),
+                Vector3::new(0.0, -1.0, 0.0),
+            );
+            let v1 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(-1.0, 0.0, 0.0),
+                Vector3::new(0.0, -1.0, 0.0),
+            );
+            let v2 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(0.0, 1.0, 0.0),
+                Vector3::new(0.0, 0.0, 1.0),
+            );
+            let v3 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(0.0, -1.0, 0.0),
+                Vector3::new(0.0, 0.0, -1.0),
+            );
+            let v4 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(0.0, 0.0, 1.0),
+                Vector3::new(0.0, -1.0, 0.0),
+            );
+            let v5 = Matrix4::look_at_dir(
+                pos,
+                Vector3::new(0.0, 0.0, -1.0),
+                Vector3::new(0.0, -1.0, 0.0),
+            );
+
+            shader.set_used();
+            shader.set_mat4("u_shadow_matrices[0]", proj * v0);
+            shader.set_mat4("u_shadow_matrices[1]", proj * v1);
+            shader.set_mat4("u_shadow_matrices[2]", proj * v2);
+            shader.set_mat4("u_shadow_matrices[3]", proj * v3);
+            shader.set_mat4("u_shadow_matrices[4]", proj * v4);
+            shader.set_mat4("u_shadow_matrices[5]", proj * v5);
+            shader.set_f32("u_far_plane", render::FAR_PLANE);
+            shader.set_vec3("u_light_pos", pos.x, pos.y, pos.z);
         }
 
         PointLight {
@@ -46,56 +97,7 @@ impl PointLight {
     }
 
     pub unsafe fn fill_depth_cubemap(&mut self, objects: &Vec<Object>) {
-        let proj = cgmath::perspective(
-            cgmath::Deg(90.0),
-            render::SHADOWMAP_SIZE as f32 / render::SHADOWMAP_SIZE as f32,
-            render::NEAR_PLANE,
-            render::FAR_PLANE,
-        );
-
-        let pos = self.position;
-
-        let v0 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(1.0, 0.0, 0.0),
-            Vector3::new(0.0, -1.0, 0.0),
-        );
-        let v1 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(-1.0, 0.0, 0.0),
-            Vector3::new(0.0, -1.0, 0.0),
-        );
-        let v2 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(0.0, 1.0, 0.0),
-            Vector3::new(0.0, 0.0, 1.0),
-        );
-        let v3 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(0.0, -1.0, 0.0),
-            Vector3::new(0.0, 0.0, -1.0),
-        );
-        let v4 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(0.0, 0.0, 1.0),
-            Vector3::new(0.0, -1.0, 0.0),
-        );
-        let v5 = Matrix4::look_at_dir(
-            pos,
-            Vector3::new(0.0, 0.0, -1.0),
-            Vector3::new(0.0, -1.0, 0.0),
-        );
-
         self.shader.set_used();
-        self.shader.set_mat4("u_shadow_matrices[0]", proj * v0);
-        self.shader.set_mat4("u_shadow_matrices[1]", proj * v1);
-        self.shader.set_mat4("u_shadow_matrices[2]", proj * v2);
-        self.shader.set_mat4("u_shadow_matrices[3]", proj * v3);
-        self.shader.set_mat4("u_shadow_matrices[4]", proj * v4);
-        self.shader.set_mat4("u_shadow_matrices[5]", proj * v5);
-        self.shader.set_f32("u_far_plane", render::FAR_PLANE);
-        self.shader.set_vec3("u_light_pos", pos.x, pos.y, pos.z);
-
         gl::Viewport(0, 0, render::SHADOWMAP_SIZE, render::SHADOWMAP_SIZE);
         gl::BindFramebuffer(gl::FRAMEBUFFER, self.fbo);
         gl::Clear(gl::DEPTH_BUFFER_BIT);
